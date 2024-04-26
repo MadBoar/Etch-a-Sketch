0d5af47a06abe3f16e21e63b3b56512ea5c4646c
Stephen <grimsey.stephen@gmail.com>
2024-04-26 02:36:03 -0400

    Added number input to change pixel counts - Pixel count automatically rounds down to the nearest root   to make a proper square

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-function screenInit() {
+function screenInit(count = 16) {
     const container = document.querySelector(".grid-container");
-    let gridCell = document.createElement("div")
-    gridCell.classList.add("cell")
-    console.log(gridCell)
+    let gridCell = document.createElement("div");
+    
+    //gridRoot to find optimal box size, used to find how many 'pixels' and their width
+    const gridRoot = Math.floor(Math.sqrt(count));
+    const pixelCount = gridRoot**2;
+    const pixelWidth = 450 / gridRoot; 
 
-    for(let i = 0; i<16; i++){
-        container.appendChild(gridCell.cloneNode(true))
+    gridCell.style.width = `${pixelWidth}px`;
+    gridCell.style.height = `${pixelWidth}px`;
+    gridCell.classList.add("cell");
+
+    
+    for(let i = 0; i < pixelCount; i++){
+        container.appendChild(gridCell.cloneNode(true));
     }
 }
+screenInit();
 
 function hoverToggle() {
     this.classList.toggle("hover");
 }
 
-screenInit();
-
+//mouse over event listener
 const grid = document.querySelectorAll(".cell")
-
 const handlerEvents = ['mouseover', 'mouseout'];
 handlerEvents.forEach(ev => {
     grid.forEach(el => {
@@ -24,3 +31,17 @@ handlerEvents.forEach(ev => {
     });
 });
 
+/*
+    Input pixel count
+    grid container adjusts to the (rounded down) square root of the pixel count
+    append a notification below the grid stating the adjusted pixel count
+
+    1000px count = sqrt(1000) rounded down => width 
+*/
+const pixelInput = document.querySelector("input");
+
+//get cell width count by squaring the 'pixel' input
+document.querySelector("button").addEventListener('click', () => {
+    const pixelCount = pixelInput.value;
+    screenInit(pixelCount);
+})
